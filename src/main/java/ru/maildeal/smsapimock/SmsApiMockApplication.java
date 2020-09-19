9f38ddfd66030cbc01194e8f48907e6ccb2fa6c0
Denis Zinovyev <zinoviev.denis@yandex.ru>
2020-09-19 20:15:22 +0300

    initial commit

---
+++ src/main/java/ru/maildeal/smsapimock/SmsApiMockApplication.java
@@ -0,0 +1,11 @@
+package ru.maildeal.smsapimock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsApiMockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApiMockApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/maildeal/smsapimock/SmsApiMockApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.maildeal.smsapimock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsApiMockApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
